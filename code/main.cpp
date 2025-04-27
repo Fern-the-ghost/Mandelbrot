804ed94ce30470f1db5d8bba45c95fb4a38dd026
Your Name <you@example.com>
2025-04-26 22:54:59 -0700

    starting main

---
+++ code/main.cpp
@@ -0,0 +1,16 @@
+#include "ComplexPlane.h"
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/System/Vector2.hpp>
+#include <iostream>
+
+int main()
+{
+    VideoMode::getDesktopMode().width / 2.0;
+    VideoMode::getDesktopMode().height/ 2.0;
+
+    RenderWindow::
+
+    return 0;
+}
